6feae70948434e0996f74a6944bd57b3a03d5714
dikac <rigel234@yahoo.com>
2022-08-21 15:01:23 +0700

    ref: remove timestamp container

--- src/timestamp/timestamp/timestamp.ts
+++
@@ -1,8 +0,0 @@
-import TimestampEntity from '../timestamp';
-import {Column} from 'typeorm';
-
-export default class Timestamp {
-
-    @Column(type=>Timestamp)
-    timestamp ?: TimestampEntity;
-}
